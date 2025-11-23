290d4aadbd8c5ebb27805078ead0ec1e2788b365
Oleksii <vazerius@gmail.com>
2025-11-23 17:00:47 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,16 @@ import styles from './page.module.css';
 
 import { Hero } from '@/src/components/hero/hero';
 import { SectionEducation } from '@/src/components/education/sectionEducation';
-
-import { SectionPrice } from '@/src/components/price/sectionPrice';
+/* 
+import { SectionPrice } from '@/src/components/price/sectionPrice'; */
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Hero />
         <SectionEducation />
-        <SectionPrice />
+        <h2>test</h2>
+        {/*         <SectionPrice /> */}
       </main>
     </div>
   );
