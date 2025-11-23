734b8cac2217db32a07399e1d7ac4b058e093492
Oleksii <vazerius@gmail.com>
2025-11-23 17:09:55 +0200

    changing fonts path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,18 @@
 import type { Metadata } from 'next';
-/* import localFont from 'next/font/local';
- */
+import localFont from 'next/font/local';
+
 import './globals.css';
 import { Header } from '@/src/components/header/header';
 
-/* const gilroy = localFont({
+const gilroy = localFont({
   src: [
-    {
-      path: '../public/fonts/Gilroy-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Gilroy-Medium.ttf',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Gilroy-Semibold.ttf',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/Gilroy-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
+    { path: './fonts/Gilroy-Regular.ttf', weight: '400', style: 'normal' },
+    { path: './fonts/Gilroy-Medium.ttf', weight: '500', style: 'normal' },
+    { path: './fonts/Gilroy-Semibold.ttf', weight: '600', style: 'normal' },
+    { path: './fonts/Gilroy-Bold.ttf', weight: '700', style: 'normal' },
   ],
   variable: '--font-gilroy',
-}); */
+});
 
 export const metadata: Metadata = {
   title: 'Genius Space',
@@ -40,9 +24,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  //      className={gilroy.variable}
   return (
-    <html lang="en" data-lt-installed="true" suppressHydrationWarning={true}>
+    <html
+      lang="en"
+      className={gilroy.variable}
+      data-lt-installed="true"
+      suppressHydrationWarning={true}
+    >
       <body>
         <Header />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import styles from './page.module.css';
 
 import { Hero } from '@/src/components/hero/hero';
 import { SectionEducation } from '@/src/components/education/sectionEducation';
-/* 
-import { SectionPrice } from '@/src/components/price/sectionPrice'; */
+import { SectionPrice } from '@/src/components/price/sectionPrice';
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -11,7 +10,7 @@ export default function Home() {
         <Hero />
         <SectionEducation />
         <h2>test</h2>
-        {/*         <SectionPrice /> */}
+        <SectionPrice />
       </main>
     </div>
   );
