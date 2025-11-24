5a1152a01a83209c3df0e957ebd14572fb99b7a5
Oleksii <vazerius@gmail.com>
2025-11-24 12:35:11 +0200

    fix-price and mobile menu

--- src/components/header/mobileMenu.tsx
+++ src/components/header/mobileMenu.tsx
@@ -1,10 +1,25 @@
 'use client';
-import { useState } from 'react';
+
+import { useState, useEffect } from 'react';
 import styles from './mobileMenu.module.css';
 import { NavigationList } from './navigation';
+import { useRouter } from 'next/navigation';
 
 export const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('menu-open');
+    } else {
+      document.body.classList.remove('menu-open');
+    }
+
+    return () => {
+      document.body.classList.remove('menu-open');
+    };
+  }, [isOpen]);
 
   return (
     <div className={styles.mobileMenu}>
@@ -21,6 +36,15 @@ export const MobileMenu = () => {
         <nav className="navigation">
           <NavigationList />
         </nav>
+        <button
+          className="button-course"
+          onClick={() => {
+            setIsOpen(false);
+            router.push('#course');
+          }}
+        >
+          Записатися на курс
+        </button>
       </div>
     </div>
   );
